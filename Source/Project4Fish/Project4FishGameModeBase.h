27c0c33a5371a1d2a96da90101f81b5a37a604a9
グローバルユーザー情報を使用しないでください <>
2026-04-09 16:03:49 +0900

    Initial Commit

---
+++ Source/Project4Fish/Project4FishGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Project4FishGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT4FISH_API AProject4FishGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
